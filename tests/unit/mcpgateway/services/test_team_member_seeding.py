64589fdd0a7237e2ae88bd4af8839bb9171ab3ec
Marek Dano <Marek.Dano@ibm.com>
2026-07-16 19:51:53 +0100

    fix: pre-commit check on CI
    Signed-off-by: Marek Dano <Marek.Dano@ibm.com>

--- tests/unit/mcpgateway/services/test_team_member_seeding.py
+++ tests/unit/mcpgateway/services/test_team_member_seeding.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """Location: ./tests/unit/mcpgateway/services/test_team_member_seeding.py
-Copyright 2026
+Copyright contributors to the MCP-CONTEXT-FORGE project
 SPDX-License-Identifier: Apache-2.0
-Authors: Mihai Criveti
 
 Tests for seeding members into a team at creation time.
 
