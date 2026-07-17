8941b6a5617e22a7a8420b19cfe3bd1c9ca51f41
YAMON.IO <battlecruser@yamon.io>
2026-07-17 21:41:42 +0900

    test(auth): keep identity regression diff focused
    Signed-off-by: YAMON.IO <battlecruser@yamon.io>

--- tests/unit/mcpgateway/transports/test_streamablehttp_transport.py
+++ tests/unit/mcpgateway/transports/test_streamablehttp_transport.py
@@ -12302,11 +12302,7 @@ async def test_get_request_context_no_request_object(monkeypatch, caplog):
     from unittest.mock import PropertyMock
 
     # First-Party
-    from mcpgateway.transports.streamablehttp_transport import (
-        _get_request_context_or_default,
-        mcp_app,
-        server_id_var,
-    )
+    from mcpgateway.transports.streamablehttp_transport import _get_request_context_or_default, mcp_app, server_id_var
 
     token = server_id_var.set("default_server_id")
 
@@ -12331,11 +12327,7 @@ async def test_get_request_context_stateful_success(monkeypatch):
     from unittest.mock import PropertyMock
 
     # First-Party
-    from mcpgateway.transports.streamablehttp_transport import (
-        _get_request_context_or_default,
-        mcp_app,
-        server_id_var,
-    )
+    from mcpgateway.transports.streamablehttp_transport import _get_request_context_or_default, mcp_app, server_id_var
 
     token = server_id_var.set("default_server_id")
 
